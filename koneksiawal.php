da9d410059cb14faab30372af027b5ac3d7d1636
alnobeta <alnobeta.risqi@gmail.com>
2016-05-02 23:48:23 +0700

    Update koneksiawal.php
    ganti koneksi

--- koneksiawal.php
+++ koneksiawal.php
@@ -1,7 +1,7 @@
 <?php
-	$namahost = "localhost";
-	$user = "root";
-	$password = "";
+	$namahost = "ap-cdbr-azure-southeast-b.cloudapp.net";
+	$user = "b1593d69c89b34";
+	$password = "26a41baa";
 	$database = "sigetek";
 	mysql_connect($namahost,$user,$password) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");

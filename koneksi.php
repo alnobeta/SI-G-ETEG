658c56655ef34a58315f302c54a5695a74fde652
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 05:16:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,8 +3,8 @@
 
 	if ($_SESSION["username"] == 'admin') {
 		$namahost = "ap-cdbr-azure-east-c.cloudapp.net";
-		$user = "admin";
-		$password = "admin";
+		$user = "badeb5841ddff5";
+		$password = "b46ffbd0";
 		$database = "sigetek";
 		mysql_connect($namahost,$user,$password,$database) or die("Failed");
 		mysql_select_db($database) or die("Database not exist");

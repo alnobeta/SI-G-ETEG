e808bcc97dbe5a1f0d18f3b53eda693abad44cbb
alnobeta <alnobeta.risqi@gmail.com>
2016-05-03 23:39:37 +0700

    Update koneksiadmin.php

--- koneksiadmin.php
+++ koneksiadmin.php
@@ -3,6 +3,6 @@
 	$user = "admin";
 	$password = "admin";
 	$database = "sigetek";
-	mysql_connect($namahost,$user,$password) or die("Failed");
+	mysql_connect($namahost,$user,$password,$database) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");
 ?>

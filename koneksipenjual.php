90e8dfa42889a47802acf051ddfe92d0faf40f5a
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 05:22:01 +0700

    Update koneksipenjual.php

--- koneksipenjual.php
+++ koneksipenjual.php
@@ -1,8 +1,8 @@
 <?php
-	$namahost = "ap-cdbr-azure-east-c.cloudapp.net";
-	$user = "penjual";
-	$password = "penjual";
-	$database = "sigetek";
+$namahost = "ap-cdbr-azure-east-c.cloudapp.net";
+		$user = "badeb5841ddff5";
+		$password = "b46ffbd0";
+		$database = "sigetek";
 	mysql_connect($namahost,$user,$password) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");
 ?>

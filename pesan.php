b5051ef093c7b63e1547be34b30e0e2dac842afb
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 12:00:03 +0700

    Update pesan.php

--- pesan.php
+++ pesan.php
@@ -10,8 +10,10 @@
 	$id = $_POST['idpesanan'];
 	$banyak = $_POST['banyak'];
 	$nomor = $_COOKIE['orderan'];
+	$web= "http://g-eteg.azurewebsites.net/";
 	$url = $_POST['urlaktif'];
-	echo $url;
+	$fix = $web.$url;
+	echo $fix;
 
 	$q1 = mysql_fetch_array(mysql_query("SELECT * FROM produk pr JOIN penjual p ON pr.id_penjual = p.id_penjual JOIN kategori k ON pr.id_kategori = k.id_kategori WHERE pr.id_produk = '$id'"));
 
@@ -20,12 +22,12 @@
 
 	if ($hitungStok < 0) {
 		setcookie("pesanErr", " Stok Tidak Tersedia Untuk Pemesanan Sebanyak $banyak kg", time()+3);
-		header("location:http://g-eteg.azurewebsites.net/$url#popup");
+		header("location:$url#popup");
 	}
 	else{
 		$ins = mysql_query("INSERT INTO pemesanan(id_pembeli,id_produk,nomor_pemesanan,nama_produk,banyak,total_harga) VALUES('".$idpembeli."','".$id."','".$nomor."','".$q1['nama_produk']."','".$banyak."','".$totalHarga."')");
 		if ($ins) {
-			header("location:http://g-eteg.azurewebsites.net/$url");
+			header("location:$url");
 		}
 		else{
 			mysql_error();

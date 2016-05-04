d02cdeca591ada252e0672c2361e49759adb9f47
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 10:38:11 +0700

    Update imageview.php

--- imageview.php
+++ imageview.php
@@ -1,13 +1,13 @@
 <?php
     include_once 'koneksi.php';
     if(isset($_GET['image_id'])) {
-        $sql = "SELECT * FROM produk WHERE id_produk=" . $_GET['image_id'];
+        $sql = "SELECT * FROM produk WHERE id_produk=".$_GET['image_id'];
 		$result = mysql_query("$sql")
-		echo "tes";
+		 or die("<b>Error:</b> Problem on Retrieving Image BLOB<br/>" . mysql_error());
         	$row = mysql_fetch_array($result);
 		//header("Content-type: " . $row["imageType"]);
         echo $row["gambar_produk"];
-        or die("<b>Error:</b> Problem on Retrieving Image BLOB<br/>" . mysql_error());
+       
 	
 	}
 	mysql_close($conn);

e567929bbfd84564156a387663a042a20579c8ae
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 14:21:35 +0700

    Update cekusername.php

--- validasi/cekusername.php
+++ validasi/cekusername.php
@@ -1,7 +1,7 @@
 <?php
-	$namahost = "localhost";
-	$user = "root";
-	$password = "";
+	$namahost = "ap-cdbr-azure-east-c.cloudapp.net";
+	$user = "badeb5841ddff5";
+	$password = "b46ffbd0";
 	$database = "sigetek";
 	mysql_connect($namahost,$user,$password) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");

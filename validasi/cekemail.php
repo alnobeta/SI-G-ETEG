5f9318b977b1f50615f4c617970a3aa848575c44
alnobeta <alnobeta.risqi@gmail.com>
2016-05-04 14:22:21 +0700

    Update cekemail.php

--- validasi/cekemail.php
+++ validasi/cekemail.php
@@ -1,7 +1,8 @@
 <?php
-	$namahost = "localhost";
-	$user = "root";
-	$password = "";
+$namahost = "ap-cdbr-azure-east-c.cloudapp.net";
+	$user = "badeb5841ddff5";
+	$password = "b46ffbd0";
+	$database = "sigetek";
 	$database = "sigetek";
 	mysql_connect($namahost,$user,$password) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");
